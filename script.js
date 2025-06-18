2262dedbfc0ce32be36daa4887addffd90196a39
s241009KOUKI <s241009@kashiwanoha.ed.jp>
2025-06-18 06:46:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// function sayHello(){
+//  console.log(greeting);
+// }
+// sayHello();
+//上はgreetingをただのに""がなくgreetingが
+
+
+function calculateArea(){
+  const width =5;
+  const height =10;
+  return width * x;
+}
+
+const area =calculateArea();
+console.log("Area:",area);
